08700dcedf643ba9782a486189cee86774365341
Valentina Kolesnikova <83814517+Valentina810@users.noreply.github.com>
2023-09-12 22:56:58 +0300

    feat: добавить заголовки Access-Control-Allow-Headers и Access-Control-Allow-Methods (#18)
    Co-authored-by: valentinakolesnikova <polikarpova-v@yandex.ru>

--- src/main/java/com/valentinakole/lms/util/AddResponseHeaderFilter.java
+++ src/main/java/com/valentinakole/lms/util/AddResponseHeaderFilter.java
@@ -19,6 +19,8 @@ public class AddResponseHeaderFilter implements Filter {
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
         httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
         httpServletResponse.setHeader("Content-Type", "application/json");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE,OPTIONS");
         chain.doFilter(request, response);
     }
 }
