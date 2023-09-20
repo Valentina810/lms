0553d1ecbae1e45ed203317d7e4c4aedaf031cb4
Valentina Kolesnikova <83814517+Valentina810@users.noreply.github.com>
2023-09-21 00:14:16 +0300

    Добавлены и исправлены валидации к полям LessonCreateDto: theoryUrl, practiceUrl, homeworkUrl, progress (#69)
    * fix: добавить валидацию к полям theoryUrl, practiceUrl, homeworkUrl в LessonCreateDto. Изменить аннотацию валидации у поля avatarUrl в UserRequestDto
    
    * fix: добавить валидацию на поле progress в LessonCreateDto
    
    ---------
    
    Co-authored-by: valentinakolesnikova <polikarpova-v@yandex.ru>

--- src/main/java/com/valentinakole/lms/dto/lesson/LessonCreateDto.java
+++ src/main/java/com/valentinakole/lms/dto/lesson/LessonCreateDto.java
@@ -2,6 +2,7 @@ package com.valentinakole.lms.dto.lesson;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.valentinakole.lms.model.Subject;
+import com.valentinakole.lms.util.validate.ValidationMessage;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
@@ -14,6 +15,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.URL;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -50,21 +52,24 @@ public class LessonCreateDto {
     @Schema(description = "Время окончания урока", example = "09:40:00")
     private LocalTime timeEnd;
 
-    @Size(max = 100, message = "Длина адреса должна быть не больше 1000 знаков")
+    @Size(max = 1000, message = "Длина адреса должна быть не больше 1000 знаков")
+    @URL(message = "Неправильный формат URL, пример https://habr.com/")
     @Schema(description = "Ссылка на теорию к уроку", example = "https://habr.com/ru/theoryUrl.htm")
     private String theoryUrl;
 
-    @Size(max = 100, message = "Длина адреса должна быть не больше 1000 знаков")
+    @Size(max = 1000, message = "Длина адреса должна быть не больше 1000 знаков")
+    @URL(message = "Неправильный формат URL, пример https://habr.com/")
     @Schema(description = "Ссылка на практику к уроку", example = "https://habr.com/ru/practiceUrl.htm")
     private String practiceUrl;
 
-    @Size(max = 100, message = "Длина адреса должна быть не больше 1000 знаков")
+    @Size(max = 1000, message = "Длина адреса должна быть не больше 1000 знаков")
+    @URL(message = "Неправильный формат URL, пример https://habr.com/")
     @Schema(description = "Ссылка на домашнюю работу к уроку", example = "https://habr.com/ru/homeworkUrl.htm")
     private String homeworkUrl;
 
-    @PositiveOrZero(message = "Прогресс должен быть числом от 0 до 100")
+    @PositiveOrZero(message = ValidationMessage.VALIDATION_PROGRESS_MESSAGE)
     @Schema(description = "Прогресс выполнения в % от 1 до 100", example = "10")
-    @Max(100)
+    @Max(value = 100, message = ValidationMessage.VALIDATION_PROGRESS_MESSAGE)
     private Integer progress;
 
     @Schema(description = "Отметка о выполнении урока", example = "false")
