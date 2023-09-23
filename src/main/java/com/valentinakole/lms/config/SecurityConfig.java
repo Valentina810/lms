a7821ef2ebf2dbf9d51bb12372a1cd85d06b8d92
Roman Yudin <yudiol114@gmail.com>
2023-09-23 22:42:38 +0300

    feat: добавить BCryptPasswordEncoder

--- src/main/java/com/valentinakole/lms/config/SecurityConfig.java
+++ src/main/java/com/valentinakole/lms/config/SecurityConfig.java
@@ -11,7 +11,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
@@ -27,8 +28,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/users/**").hasAuthority(Role.USER.toString())
-                        .anyRequest().authenticated()
-                )
+                        .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
         return http.build();
@@ -43,7 +43,12 @@ public class SecurityConfig {
     DaoAuthenticationProvider daoAuthenticationProvider() {
         DaoAuthenticationProvider dao = new DaoAuthenticationProvider();
         dao.setUserDetailsService(userDetailService);
-        dao.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        dao.setPasswordEncoder(passwordEncoder());
         return dao;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
