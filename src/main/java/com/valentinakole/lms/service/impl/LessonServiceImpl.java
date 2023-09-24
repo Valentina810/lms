2ecd8a1ac2012607aca0620d46402389e735120a
valentinakolesnikova <polikarpova-v@yandex.ru>
2023-09-24 23:03:15 +0300

    fix: перенести проверку наличия пользователя в getLessons - выполняется только в случае, если вернулся пустой массив уроков

--- src/main/java/com/valentinakole/lms/service/impl/LessonServiceImpl.java
+++ src/main/java/com/valentinakole/lms/service/impl/LessonServiceImpl.java
@@ -37,14 +37,13 @@ public class LessonServiceImpl implements LessonService {
     @Override
     public FullLessonDto getLesson(long userId, long lessonId) {
         FullLessonDto fullLessonDto = lessonMapper.toFullLessonDto(lessonRepository.findLessonByUserId(userId, lessonId).orElseThrow(() ->
-                new NotFoundException("У пользователя с id "+userId+" не найден урок с id "+lessonId)));
+                new NotFoundException("У пользователя с id " + userId + " не найден урок с id " + lessonId)));
         log.info("Получен урок {}", fullLessonDto);
         return fullLessonDto;
     }
 
     @Override
     public List<ShortLessonDto> getLessons(long userId, LocalDate from, LocalDate to) {
-        userRepository.findById(userId).orElseThrow(() -> new NotFoundException("Пользователь", userId));
         if (from == null) {
             from = LocalDate.now().minusDays(LocalDate.now().getDayOfWeek().getValue() - 1);
         }
@@ -55,6 +54,9 @@ public class LessonServiceImpl implements LessonService {
                 .stream()
                 .map(lessonMapper::toShortLessonDto)
                 .collect(Collectors.toList());
+        if (shortLessonDtos.size() == 0) {
+            userRepository.findById(userId).orElseThrow(() -> new NotFoundException("Пользователь", userId));
+        }
         log.info("Получены уроки пользователя c id {} за период с {} по {}: {}", userId, from, to, shortLessonDtos);
         return shortLessonDtos;
     }
