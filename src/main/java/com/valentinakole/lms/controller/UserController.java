b6bb2a162efe8eb53bf797873cb6dc5b81b72418
Roman Yudin <yudiol114@gmail.com>
2023-10-03 22:58:04 +0300

    fix: изменить annotation *

--- src/main/java/com/valentinakole/lms/controller/UserController.java
+++ src/main/java/com/valentinakole/lms/controller/UserController.java
@@ -15,7 +15,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
