a2fe00a12b1fee5567ce1ee57f0d015e1d9ba50e
valentinakolesnikova <polikarpova-v@yandex.ru>
2023-11-18 23:38:12 +0300

    feat: добавить юнит-тесты для LessonService

--- src/main/java/com/valentinakole/lms/model/Lesson.java
+++ src/main/java/com/valentinakole/lms/model/Lesson.java
@@ -9,7 +9,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
@@ -19,6 +22,9 @@ import java.time.LocalTime;
 @Table(name = "lessons", schema = "public")
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Lesson {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
