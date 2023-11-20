941fd2ca773c48e49e94dc5883e7f63b02debf18
Valentina Kolesnikova <83814517+Valentina810@users.noreply.github.com>
2023-11-20 20:23:52 +0300

    Добавить юнит-тесты, сгенерированные ChatGpt4 (#109)
    * feat: добавить юнит-тесты для LessonController
    
    * feat: добавить юнит-тесты для LessonService
    
    * feat: добавить юнит-тесты для LessonRepository
    
    ---------
    
    Co-authored-by: valentinakolesnikova <polikarpova-v@yandex.ru>

--- src/main/java/com/valentinakole/lms/model/Subject.java
+++ src/main/java/com/valentinakole/lms/model/Subject.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +14,7 @@ import lombok.Setter;
 @Table(name = "subjects", schema = "public")
 @Getter
 @Setter
+@EqualsAndHashCode
 public class Subject {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
